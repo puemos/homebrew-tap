3fb1723485fed2c1d5fa8a8a084b3274675ceeba
Shy Alter <puemos@gmail.com>
2026-01-08 21:04:54 +0100

    Fix cask URL format
    Assets don't have version in filename: LaReview_aarch64.app.tar.gz
    not LaReview_0.0.18_aarch64.app.tar.gz

--- Casks/lareview.rb
+++ Casks/lareview.rb
@@ -5,7 +5,7 @@ cask "lareview" do
   sha256 arm:   "6135b32d3bb596aaa2b45e69e35e7223ff00d1dfc767397a0f87c80d8d08d2f3",
          intel: "7eda332cd3a951bed684b2d2edab92c06ec74d7d9e0d3024bf7e765379888c4f"
 
-  url "https://github.com/puemos/lareview/releases/download/v#{version}/LaReview_#{version}_#{arch}.app.tar.gz"
+  url "https://github.com/puemos/lareview/releases/download/v#{version}/LaReview_#{arch}.app.tar.gz"
   name "LaReview"
   desc "Desktop code review companion with AI-generated plans"
   homepage "https://github.com/puemos/lareview"
